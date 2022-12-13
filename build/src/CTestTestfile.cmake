ed942b88ad80d0897168026bc61ea61d049a496e
mattyadada <matthewmc96@gmail.com>
2022-12-12 23:55:31 -0500

    wip

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/monte/Desktop/CPP-Projects/KDE_APP/src
-# Build directory: /home/monte/Desktop/CPP-Projects/KDE_APP/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
